b6a604fc4aaff5776d921f17288ee8342fde6617
Denis Moskowitz <denismm@gmail.com>
2022-11-07 20:21:01 -0500

    black

--- 2016/5b.py
+++ 2016/5b.py
@@ -5,7 +5,7 @@ import hashlib
 door_id = sys.argv[1]
 
 i = 0
-password = ['.'] * 8
+password = ["."] * 8
 preamble = "00000"
 found = 0
 while found < 8:
@@ -13,9 +13,9 @@ while found < 8:
     hash = hashlib.md5(candidate.encode()).hexdigest()
     if hash.startswith(preamble):
         position = hash[5]
-        if position <= '7':
+        if position <= "7":
             position = int(position)
-            if password[position] == '.':
+            if password[position] == ".":
                 password[position] = hash[6]
                 print(f"password {''.join(password)} found at {i}")
                 found += 1
