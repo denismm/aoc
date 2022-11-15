c328f5ac669582deca4170dff3a91577bddb78f8
Denis Moskowitz <denismm@gmail.com>
2022-11-14 20:18:27 -0500

    black 7a

--- 2016/7a.py
+++ 2016/7a.py
@@ -3,11 +3,15 @@ import sys
 import re
 
 filename = sys.argv[1]
-section_re = re.compile(r'([a-z]*)(?:\[([a-z]*)\])?')
-abba_re = re.compile(r'(.)(.)\2\1')
+section_re = re.compile(r"([a-z]*)(?:\[([a-z]*)\])?")
+abba_re = re.compile(r"(.)(.)\2\1")
+
+
 def is_abba(sequence):
     m = abba_re.search(sequence)
     return m and m.group(1) != m.group(2)
+
+
 with open(filename, "r") as f:
     tls_count = 0
     for line in f:
@@ -19,9 +23,8 @@ with open(filename, "r") as f:
             if is_abba(base):
                 base_abba = True
             if is_abba(hyper):
-               hyper_abba = True
+                hyper_abba = True
         tls = base_abba and not hyper_abba
         if tls:
             tls_count += 1
     print(tls_count)
-        
