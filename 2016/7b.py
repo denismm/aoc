1206dcdd2f084f5f83b624acff364f29c99e1a84
Denis Moskowitz <denismm@gmail.com>
2022-11-14 20:45:29 -0500

    black 7b

--- 2016/7b.py
+++ 2016/7b.py
@@ -6,10 +6,11 @@ filename = sys.argv[1]
 section_re = re.compile(r"([a-z]*)(?:\[([a-z]*)\])?")
 abba_re = re.compile(r"(.)(.)\2\1")
 
+
 def get_aba(sequence):
     for i in range(len(sequence) - 2):
-        if sequence[i] == sequence[i+2] != sequence[i+1]:
-            yield (sequence[i], sequence[i+1])
+        if sequence[i] == sequence[i + 2] != sequence[i + 1]:
+            yield (sequence[i], sequence[i + 1])
 
 
 with open(filename, "r") as f:
