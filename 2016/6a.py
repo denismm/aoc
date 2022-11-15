38abcefe5cb0ea8008d9eb9e6d9847a21719a504
Denis Moskowitz <denismm@gmail.com>
2022-11-14 19:38:10 -0500

    6

---
+++ 2016/6a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+import collections
+
+filename = sys.argv[1]
+with open(filename, "r") as f:
+    counts = []
+    for line in f:
+        for (i, letter) in enumerate(line.rstrip()):
+            if len(counts) <= i:
+                counts.append(collections.defaultdict(lambda: 0))
+            counts[i][letter] += 1
+    answer = []
+    for c in counts:
+        top = max(c.values())
+        tops = [k for k,v in c.items() if v == top]
+        if len(tops) != 1:
+            raise ValueError(f'no single top: {tops}')
+        answer.append(tops[0])
+    print ("".join(answer))
