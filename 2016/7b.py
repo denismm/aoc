1b7192f1f7e3ab039de57a5905991607fa6801d1
Denis Moskowitz <denismm@gmail.com>
2022-11-14 20:41:22 -0500

    7b

---
+++ 2016/7b.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+filename = sys.argv[1]
+section_re = re.compile(r"([a-z]*)(?:\[([a-z]*)\])?")
+abba_re = re.compile(r"(.)(.)\2\1")
+
+def get_aba(sequence):
+    for i in range(len(sequence) - 2):
+        if sequence[i] == sequence[i+2] != sequence[i+1]:
+            yield (sequence[i], sequence[i+1])
+
+
+with open(filename, "r") as f:
+    ssl_count = 0
+    for line in f:
+        base_aba = set()
+        hyper_bab = set()
+        sections = section_re.findall(line.rstrip())
+        for section in sections:
+            (base, hyper) = section
+            for aba in get_aba(base):
+                base_aba.add(aba)
+            for aba in get_aba(hyper):
+                hyper_bab.add(tuple(reversed(aba)))
+        ssl = base_aba & hyper_bab
+        if ssl:
+            ssl_count += 1
+    print(ssl_count)
