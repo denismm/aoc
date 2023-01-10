014e459cc953b93dd8ac29862854210107768074
Denis Moskowitz <denismm@gmail.com>
2023-01-09 20:44:05 -0500

    17

---
+++ 2016/17a.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import sys
+import collections
+import hashlib
+
+seed: str = sys.argv[1]
+
+Position = tuple[int, ...]
+start: Position = (0, 0)
+destination: Position = (3, 3)
+directions: dict[str, Position] = {
+        "U": (-1, 0),
+        "D": (1, 0),
+        "L": (0, -1),
+        "R": (0, 1),
+    }
+
+dir_list = "UDLR"
+
+def add_pos(position: Position, dir: Position) -> Position:
+    return tuple([p + d for p, d in zip(position, dir)])
+
+State = tuple[Position, str]    # second is path to here
+
+queue: collections.deque[State] = collections.deque()
+queue.append( (start, "") )
+current_distance = -1
+
+while len(queue):
+    this_state = queue.popleft()
+    position, path = this_state
+    if len(path) > current_distance:
+        current_distance = len(path)
+        print(len(queue), len(path))
+    hash_input = seed + path
+    hash = hashlib.md5(hash_input.encode()).hexdigest()
+    for (dir, door) in zip(dir_list, hash[:4]):
+        if door in 'bcdef':
+            new_position = add_pos(position, directions[dir])
+            new_path = path + dir
+            if new_position == destination:
+                print(new_path)
+                exit(0)
+            if 0 <= new_position[0] < 4 and 0 <= new_position[1] < 4:
+                queue.append( (new_position, new_path))
+
+print("no exit")
