3f98367f61d05b92a3af9b9d9fb724d03eab4c00
Denis Moskowitz <denismm@gmail.com>
2023-06-05 19:25:24 -0400

    11a

---
+++ 2017/11a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import sys
+from positions import Position, add_direction
+
+filename = sys.argv[1]
+
+directions: dict[str, Position] = {
+    'n' : ( 0, -1),
+    'ne': ( 1, -1),
+    'se': ( 1,  0),
+    's' : ( 0,  1),
+    'sw': (-1,  1),
+    'nw': (-1,  0),
+}
+
+with open(filename, 'r') as f:
+    for line in f:
+        steps = line.rstrip().split(',')
+        location: Position = (0, 0)
+        for step in steps:
+            step_direction = directions[step]
+            location = add_direction(location, step_direction)
+        (q, r) = location
+        s: int = -q - r
+        distance = (abs(q) + abs(r) + abs(s)) // 2
+        print(distance)
