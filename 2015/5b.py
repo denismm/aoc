26b8df51c36e1421d82ddb843b94aa77c83321ce
Denis Moskowitz <denismm@gmail.com>
2023-11-20 20:42:55 -0500

    day 5

---
+++ 2015/5b.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+nice_rules = [ re.compile(rule) for rule in (r'(..).*\1', r'(.).\1')]
+
+def is_nice(line: str) -> bool:
+    for rule in nice_rules:
+        if not rule.search(line):
+            return False
+    return True
+
+
+filename = sys.argv[1]
+nice: int = 0
+with open(filename, 'r') as f:
+    for line in f:
+        if is_nice(line):
+            nice += 1
+print(nice)
