cbb77ef3b1e042b3a27d98e74a0635ea0c19fdbd
Denis Moskowitz <denismm@gmail.com>
2023-12-13 19:18:57 -0500

    trim args to recursive count_valid calls

--- 2023/12a.py
+++ 2023/12a.py
@@ -10,28 +10,6 @@ else:
 
 total_counts = 0
 
-def get_arrangement(springs: list[str], combination: tuple[int, ...]) -> list[str]:
-    output: list[str] = []
-    combo_i = 0
-    for char in springs:
-        if char == '?':
-            if combo_i in combination:
-                new_char = "#"
-            else:
-                new_char = " "
-            combo_i += 1
-        elif char == ".":
-            new_char = " "
-        else:
-            new_char = char
-        output.append(new_char)
-    return output
-
-def get_groups(arrangement: list[str]) -> list[int]:
-    springs = ''.join(arrangement)
-    groups = [len(x) for x in springs.split()]
-    return groups
-
 def count_valids(
         springs: list[str],
         groups: list[int],
@@ -39,21 +17,22 @@ def count_valids(
     # print(f"cv:\t>{''.join(springs)}< {groups} {unknown_locs}")
     valid_count = 0
     position = unknown_locs[0]
-    new_springs = list(springs)
+    work_springs = list(springs)
     for choice in ['#', ' ']:
-        new_springs[position] = choice
+        work_springs[position] = choice
         # check validity so far
-        unknowns = sum([1 for c in new_springs if c == '?'])
-        knowns = sum([1 for c in new_springs if c == '#'])
+        unknowns = sum([1 for c in work_springs if c == '?'])
+        knowns = sum([1 for c in work_springs if c == '#'])
         spring_count = sum(groups)
         if not (knowns <= spring_count <= knowns + unknowns):
             # print(f"can't continue with {knowns=} {unknowns=} {spring_count=}")
             continue
-        spring_groups = ''.join(new_springs).split()
+        spring_groups = ''.join(work_springs).split()
         invalid = False
         full_scan = True
-        # print(f"check:\t>{''.join(new_springs)}<")
+        # print(f"check:\t>{''.join(work_springs)}<")
         for spring_group, group in zip_longest(spring_groups, groups):
+            # print(f"{spring_group=} {group=}")
             if spring_group is None:
                 # not enough spaces left for numbers
                 # print('not enough spaces left')
@@ -65,6 +44,10 @@ def count_valids(
                     # print('uncounted springs')
                     invalid = True
                     break
+            # "##?" is invalid for a group of 1, etc.
+            if group and spring_group.startswith('#' * (group + 1)):
+                invalid = True
+                break
             if '?' in spring_group:
                 full_scan = False
                 break
@@ -81,7 +64,28 @@ def count_valids(
             valid_count += 1
         if len(unknown_locs) > 1:
             # we have more groups to consider
-            valid_count += count_valids(new_springs, groups, unknown_locs[1:])
+            # transform to smaller problem
+            next_position = unknown_locs[1]
+            # leave one previous character to anchor any in-progress group
+            trim_chars = next_position - 1
+            next_unknowns = [l - trim_chars for l in unknown_locs[1:]]
+            handled_springs = work_springs[:trim_chars]
+            next_springs = work_springs[trim_chars:]
+            seen_groups = [len(group) for group in ''.join(handled_springs).split()]
+            # are we subtracting from last set?
+            subtraction = 0
+            if handled_springs and handled_springs[-1] == '#' and next_springs[0] == '#':
+                subtraction = seen_groups.pop()
+
+            handled_groups = groups[:len(seen_groups)]
+            if handled_groups != seen_groups:
+                raise ValueError(f"{handled_groups=} != {seen_groups=}")
+            next_groups = groups[len(seen_groups):]
+            # print(f"{next_groups=} {next_unknowns=} {subtraction=}")
+            if subtraction:
+                next_groups[0] -= subtraction
+
+            valid_count += count_valids(next_springs, next_groups, next_unknowns)
     # print(f"returning {valid_count}")
     return valid_count
 
@@ -91,7 +95,7 @@ with open(filename, 'r') as f:
         spring_info = spring_info.replace('.', ' ')
         # print(spring_info)
         unfolded_spring_info = '?'.join([spring_info] * multiplier)
-        springs = list(unfolded_spring_info)
+        springs = [' '] + list(unfolded_spring_info)
         groups = [int(s) for s in group_info.split(',')] * multiplier
         unknown_locs = [i for i, char in enumerate(springs) if char == '?']
         valid_arrangements = count_valids(springs, groups, unknown_locs)
