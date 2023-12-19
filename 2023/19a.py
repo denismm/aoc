a30a45eabb63fdf8b522f3b7634e34ef84701ae5
Denis Moskowitz <denismm@gmail.com>
2023-12-19 10:58:34 -0500

    parse

--- 2023/19a.py
+++ 2023/19a.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import sys
 from typing import NamedTuple
+from pprint import pprint
+import re
 
 filename = sys.argv[1]
 
 # default rule has field of "" and comparison of ""
 Rule = NamedTuple('Rule', [('field', str), ('comparison', str), ('value', int), ('dest', str)])
+
+rule_re = re.compile(r'(\w+)([<=>])(\d+):(\w+)')
 workflows: dict[str, list[Rule]] = {}
 
 Item = dict[str, int]
@@ -15,11 +19,22 @@ with open(filename, "r") as f:
     for line in f:
         line = line.rstrip()
         if line.startswith('{'):
-            print(line.strip('{}').split(','))
-            item = {field: 0 
-                for field in line.strip('{}').split(',')
-                for (k, v) in field.split('=') 
-            }
+            fields = line.strip('{}').split(',')
+            item: Item = {}
+            for field in fields:
+                k, v = field.split('=')
+                item[k] = int(v)
             items.append(item)
+        elif line:
+            name, rules_s = line.split('{')
+            rules: list[Rule] = []
+            for entry in rules_s.rstrip('}').split(','):
+                m = rule_re.match(entry)
+                if m:
+                    field, comparison, value_s, dest = m.groups()
+                    rules.append(Rule(field, comparison, int(value_s), dest))
+                else:
+                    rules.append(Rule("", "", 0, entry))
+            workflows[name] = rules
 
-print(items)
+pprint(workflows)
