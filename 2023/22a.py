3180675e59930507212a906388f5fb144b80f7cc
Denis Moskowitz <denismm@gmail.com>
2023-12-22 08:53:18 -0500

    precalculate supports

--- 2023/22a.py
+++ 2023/22a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 from positions import Position, add_direction
+from pprint import pprint
 
 filename = sys.argv[1]
 
@@ -31,36 +32,42 @@ def make_brick(fbrick: FlyingBrick) -> Brick:
 
 pile: Grid = {}
 bricks: list[Brick] = []
+# for each brick, what bricks are directly below it?
+support_for_brick: dict[int, set[int]] = {}
+supported_by_brick: dict[int, set[int]] = {}
 
 # drop bricks into pile
 for i, fbrick in enumerate(flying_bricks):
     # lower until supported in pile
     brick = make_brick(fbrick)
     # don't drop it if it's at z=1
+    supporters: set[int] = set()
     while fbrick[0][2] > 1:
         new_brick = {add_direction(pos, down) for pos in brick}
-        if new_brick & set(pile.keys()):
+        collisions = new_brick & set(pile.keys())
+        if collisions:
+            for pos in collisions:
+                supporter = pile[pos]
+                supporters.add(supporter)
+                supported_by_brick[supporter].add(i)
             break
         brick = new_brick
         fbrick = tuple(add_direction(pos, down) for pos in fbrick)
     for position in brick:
         pile[position] = i
     bricks.append(brick)
+    support_for_brick[i] = supporters
+    supported_by_brick[i] = set()
 
 # for each brick, check supportiveness
 
 free_bricks = 0
-for i, brick in enumerate(bricks):
-    supported_positions = { add_direction(pos, up) for pos in brick }
-    supported_positions -= brick
-    supported_bricks = {pile[pos] for pos in supported_positions if pos in pile}
+for i in range(len(bricks)):
+    supported_bricks = supported_by_brick[i]
     removable = True
     for brick_no in supported_bricks:
-        supported = bricks[brick_no]
-        supporting_positions = {add_direction(pos, down) for pos in supported}
-        supporting_positions -= supported
-        supporting_positions -= brick
-        if not supporting_positions & set(pile.keys()):
+        supporters = support_for_brick[brick_no] - {i}
+        if not supporters:
             # I don't think we need to worry about bricks on the ground
             removable = False
             break
