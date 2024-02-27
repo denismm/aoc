d0696e0d1088a0ec4a3f0869725505c54928522c
Denis Moskowitz <denismm@gmail.com>
2024-02-26 19:38:02 -0500

    12

---
+++ 2015/12b.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import sys
+import json
+from typing import Any
+
+filename = sys.argv[1]
+
+with open(filename, 'r') as f:
+    data = json.loads(f.read())
+
+def sum_numbers(key: list[Any], input: dict[str, Any] | list[Any] | int) -> int:
+    total: int = 0
+    if isinstance(input, int):
+        return input
+    if isinstance(input, dict):
+        if 'red' not in input.values():
+            for k, v in sorted(input.items()):
+                total += sum_numbers(key + [k], v)
+    elif isinstance(input, list):
+        for i, v in enumerate(input):
+            total += sum_numbers(key + [i], v)
+    return total
+
+print(sum_numbers([], data))
