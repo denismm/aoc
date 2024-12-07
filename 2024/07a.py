7e703a0903c02b04f382c0e181255e895b737a7c
Denis Moskowitz <denismm@gmail.com>
2024-12-07 10:44:21 -0500

    07

---
+++ 2024/07a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import sys
+from itertools import product
+filename = sys.argv[1]
+
+operators = "+*"
+calibration = 0
+
+with open(filename, "r") as f:
+    for line in f:
+        target_s, equation_s = line.split(':')
+        target = int(target_s)
+        equation = [int(s) for s in equation_s.split()]
+        op_n = len(equation) - 1
+        start = equation.pop(0)
+        for op_seq in product(operators, repeat=op_n):
+            result = start
+            for (op, arg) in zip(op_seq, equation):
+                if op == '*':
+                    result *= arg
+                elif op == '+':
+                    result += arg
+            if result == target:
+                calibration += target
+                break
+print(calibration)
