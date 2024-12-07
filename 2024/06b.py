95e6c18e98cfab84659b01c3cc886611fdab3304
Denis Moskowitz <denismm@gmail.com>
2024-12-07 10:22:33 -0500

    better 06

--- 2024/06b.py
+++ 2024/06b.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import sys
+from tqdm import tqdm
 from positions import (
+    Position,
     read_char_grid,
     StrGrid,
     add_direction,
@@ -13,30 +15,35 @@ filename = sys.argv[1]
 with open(filename, "r") as f:
     _, _, lab = read_char_grid(f, skip_dots=False)
 
-def loop_check(lab: StrGrid) -> bool:
+class FoundLoop(Exception):
+    pass
+
+def run_lab(lab: StrGrid) -> list[Position]:
     pos = [k for k, v in lab.items() if v in direction_symbols][0]
     dir = direction_symbols.index(lab[pos])
     direction = cardinal_directions[dir]
-    lab[pos] = 'X'
     next_pos = add_direction(pos, direction)
     while next_pos in lab:
         if lab[next_pos] == '#':
             dir = (dir + 1) % 4
             direction = cardinal_directions[dir]
         elif lab[next_pos] == direction_symbols[dir]:
-            return True
+            raise FoundLoop()
         else:
             pos = next_pos
             lab[next_pos] = direction_symbols[dir]
         next_pos = add_direction(pos, direction)
-    return False
+    return [ k for k, v in lab.items() if v in direction_symbols ]
 
 obstruction_count = 0
-for obstruction in lab.keys():
+possible_obs = run_lab(dict(lab))
+for obstruction in tqdm(possible_obs):
     if lab[obstruction] == '.':
         new_lab = dict(lab)
         new_lab[obstruction] = '#'
-        if loop_check(new_lab):
+        try:
+            _ = run_lab(new_lab)
+        except FoundLoop:
             obstruction_count += 1
 
 print(obstruction_count)
