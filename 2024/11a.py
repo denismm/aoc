e9e4d0f5f94371d858f0271268fcf0ec0b762228
Denis Moskowitz <denismm@gmail.com>
2024-12-11 10:13:08 -0500

    smarter 11, handles 11b

--- 2024/11a.py
+++ 2024/11a.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 import sys
 from tqdm import tqdm
+from collections import Counter
 filename = sys.argv[1]
+blinks = int(sys.argv[2])
+
+# order doesn't matter so just track contents
+stones: Counter[int] = Counter()
 
 with open(filename, "r") as f:
-    stones: list[int] = [int(s) for s in f.read().split()]
+    in_stones: list[int] = [int(s) for s in f.read().split()]
+    stones = Counter(in_stones)
 
-blinks = 25
 
 for _ in tqdm(range(blinks)):
     # rules:
@@ -14,18 +19,18 @@ for _ in tqdm(range(blinks)):
     # even digits ABCD -> AB CD
     # *= 2024
 
-    new_stones: list[int] = []
-    addition: list[int] = []
-    for stone in stones:
+    new_stones: Counter[int] = Counter()
+    for stone, count in stones.items():
         str_stone = str(stone)
         if stone == 0:
-            addition = [1]
+            new_stones[1] += count
         elif len(str_stone) % 2 == 0:
             halfway = len(str_stone) // 2
-            addition = [ int(str_stone[:halfway]), int(str_stone[halfway:]) ]
+            addition = [ str_stone[:halfway], str_stone[halfway:] ]
+            for new_stone in addition:
+                new_stones[int(new_stone)] += count
         else:
-            addition = [stone * 2024]
-        new_stones += addition
+            new_stones[stone * 2024] += count
     stones = new_stones
     # print(stones)
-print(len(stones))
+print(sum(stones.values()))
